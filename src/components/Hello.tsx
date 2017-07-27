37804f658bbbbc5bd2738922694e340208c0757b
Jonathan Milgrom <jbmilgrom@gmail.com>
2017-07-27 11:10:36 -0400

    formatting yo

--- src/components/Hello.tsx
+++ src/components/Hello.tsx
@@ -1,21 +1,21 @@
 import * as React from 'react';
 
 export interface Props {
-    name: string;
-    enthusiasmLevel?: number;
+  name: string;
+  enthusiasmLevel?: number;
 }
 
-function Hello({ name, enthusiasmLevel = 1 }: Props) {
-    if (enthusiasmLevel <= 0) {
-        throw new Error('Please be more enthusiastic next time tho');
-    }
-    return (
-        <div className="hello">
-            <div className="greeting">
-                Hello {name + getExclamationMarks(enthusiasmLevel)}
-            </div>
-        </div>
-    );
+function Hello({name, enthusiasmLevel = 1}: Props) {
+  if (enthusiasmLevel <= 0) {
+    throw new Error('Please be more enthusiastic next time tho');
+  }
+  return (
+    <div className="hello">
+      <div className="greeting">
+        Hello {name + getExclamationMarks(enthusiasmLevel)}
+      </div>
+    </div>
+  );
 
 }
 
@@ -24,5 +24,5 @@ export default Hello;
 // private helpers
 
 function getExclamationMarks(numChars: number): string {
-    return Array(numChars + 1).join('!');
+  return Array(numChars + 1).join('!');
 }
--- src/components/MyInput.tsx
+++ src/components/MyInput.tsx
@@ -1,22 +1,23 @@
 import * as React from 'react';
 
 interface Props {
-    value: string;
-    handleChange(text: string): void;
+  value: string;
+
+  handleChange(text: string): void;
 }
 
 function MyInput({value, handleChange}: Props) {
-    const _handleChange = (event: React.FormEvent<HTMLInputElement>) => {
-      handleChange(event.currentTarget.value);
-    };
-    return (
-        <input
-            type="text"
-            placeholder={value}
-            value={value}
-            onChange={_handleChange}
-        />
-    );
+  const _handleChange = (event: React.FormEvent<HTMLInputElement>) => {
+    handleChange(event.currentTarget.value);
+  };
+  return (
+    <input
+      type="text"
+      placeholder={value}
+      value={value}
+      onChange={_handleChange}
+    />
+  );
 }
 
 export default MyInput;
