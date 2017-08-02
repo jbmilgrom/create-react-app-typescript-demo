8ac9e2758c8b663f3c682c8e949ef6487645f18e
Connor Murphy <CMurphy@triplelift.com>
2017-08-02 10:47:01 -0400

    That did work.

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import * as React from "react";
-import "./App.css";
+import * as React from 'react';
+import './App.css';
 
-import Header from "./components/Header";
-import Hello from "./components/Hello";
-import InputError from "./components/InputError";
-import MyInput from "./components/MyInput";
+import Header from './components/Header';
+import Hello from './components/Hello';
+import InputError from './components/InputError';
+import MyInput from './components/MyInput';
 
-const logo = require("./logo.svg");
-const defaultName = "Jonathan";
+const logo = require('./logo.svg');
+const defaultName = 'Jonathan';
 const defaultEnthusiasmLevel = 7;
-const defaultErrorMessage = "";
+const defaultErrorMessage = '';
 
 interface State {
   name: string;
@@ -51,19 +51,19 @@ class App extends React.Component<{}, State> {
 
   getEnthusiasmErrorMessage(level: number): string {
     if (isNaN(level)) {
-      return "Please type in a number you goose, silly.";
+      return 'Please type in a number you goose, silly.';
     }
     if (level < 1) {
-      return "Why so serious?";
+      return 'Why so serious?';
     }
     if (level > 9000) {
       return "IT\'S OVER 9000!!!!!!!!!!!!!!";
     }
     if (level > 200) {
-      return "Calm down there buddy.";
+      return 'Calm down there buddy.';
     }
     if (level != 10000) {
-      return "no way.";
+      return 'that was it.';
     }
     return defaultErrorMessage;
   }
